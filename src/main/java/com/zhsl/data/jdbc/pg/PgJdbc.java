d02436fa9d5c1f254cd90f29d01638e1e44c98e3
xiangjg <1991887681@qq.com>
2018-09-07 08:31:44 +0800

    init

--- src/main/java/com/zhsl/data/jdbc/pg/PgJdbc.java
+++ src/main/java/com/zhsl/data/jdbc/pg/PgJdbc.java
@@ -20,7 +20,7 @@ public class PgJdbc {
             Class.forName("org.postgresql.Driver");
             String strCon = "jdbc:postgresql://42.123.116.132:5432/SmartWaterDB";
             String strUserName = "postgres"; // 数据库的用户名称
-            String strPWD = "watercloud-!@#123QWE"; // 数据库的密码
+            String strPWD = ""; // 数据库的密码
             con = java.sql.DriverManager.getConnection(strCon, strUserName, strPWD);
         }catch (Exception e){
             System.out.println("加载数据库驱动失败："+e.getMessage());
