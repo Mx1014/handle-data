5d4745d40c4c636ee1ca0ec58ab2c63c3397f434
xiang <xiangjingguang@sina.com>
2019-05-13 22:53:15 +0800

    111

--- src/test/java/com/zhsl/test/MyWFExcel.java
+++ src/test/java/com/zhsl/test/MyWFExcel.java
@@ -4,6 +4,9 @@ import com.zhsl.data.entity.WFVo;
 import com.zhsl.data.jdbc.mysql.JDBCTools;
 import com.zhsl.data.util.ExcelUtil;
 import org.apache.commons.lang.StringUtils;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.junit.Test;
 
@@ -22,14 +25,14 @@ public class MyWFExcel {
             ExcelUtil eu = new ExcelUtil();
             eu.setExcelPath("D:\\查重复值.xlsx");
             eu.setSelectedSheetIdx(0);
-            List<Row> list = eu.readExcel();
+            List<Row> list0 = eu.readExcel();
             List<List<String>> myList = new ArrayList<>();
-            for (int i = 0; i < list.size(); i++) {
+            for (int i = 0; i < list0.size(); i++) {
                 if (i > 0) {
                     List<String> tmp = new ArrayList<>();
                     tmp.add(String.valueOf(i));
                     for (int j = 2; j < 4; j++) {
-                        String value = ExcelUtil.getCellValueForCell(eu.wb, list.get(i).getCell(j));
+                        String value = ExcelUtil.getCellValueForCell(eu.wb, list0.get(i).getCell(j));
                         tmp.add(value.trim());
                     }
                     myList.add(tmp);
@@ -41,14 +44,14 @@ public class MyWFExcel {
 
 
             eu.setSelectedSheetIdx(1);
-            list = eu.readExcel();
+            List<Row> list1 = eu.readExcel();
             myList = new ArrayList<>();
-            for (int i = 0; i < list.size(); i++) {
+            for (int i = 0; i < list1.size(); i++) {
                 if (i > 0) {
                     List<String> tmp = new ArrayList<>();
                     tmp.add(String.valueOf(i));
                     for (int j = 0; j < 2; j++) {
-                        String value = ExcelUtil.getCellValueForCell(eu.wb, list.get(i).getCell(j));
+                        String value = ExcelUtil.getCellValueForCell(eu.wb, list1.get(i).getCell(j));
                         tmp.add(value.trim());
                     }
                     myList.add(tmp);
@@ -57,6 +60,13 @@ public class MyWFExcel {
             //System.out.println(myList.toString());
 
             List<WFVo> oneList = getList(myList);
+
+
+            CellStyle style = eu.wb.createCellStyle();
+            style.setFillForegroundColor(IndexedColors.AQUA.getIndex());
+            style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+            style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
 
             List<Integer> ret = new ArrayList<>();
             List<WFVo> tmpList = getList(myList);
@@ -67,6 +77,10 @@ public class MyWFExcel {
                     if(obj1.getCode().equals(obj2.getCode())){
                         tmpList.add(obj1);
                         ret.add(obj1.getNum());
+                        for (int i = 0; i < 16; i++) {
+                            list0.get(obj1.getNum()-1).getCell(i).setCellStyle(style);
+                        }
+
                     }
                 }
             }
@@ -82,21 +96,21 @@ public class MyWFExcel {
                 }
             }
             System.out.println(ret.toString());
-
-            ret = new ArrayList<>();
-
-            for (WFVo obj1:oneList
-                    ) {
-                for (WFVo obj2:allList
-                        ) {
-                    if(obj1.getCode().equals(obj2.getCode())&&obj1.getName().equals(obj2.getName())){
-                        ret.add(obj1.getNum());
-                    }
-                }
-            }
-
-            System.out.println("对比编码及名称后，总表中存在表1的记录数:"+ret.size());
-            System.out.println(ret.toString());
+//
+//            ret = new ArrayList<>();
+//
+//            for (WFVo obj1:oneList
+//                    ) {
+//                for (WFVo obj2:allList
+//                        ) {
+//                    if(obj1.getCode().equals(obj2.getCode())&&obj1.getName().equals(obj2.getName())){
+//                        ret.add(obj1.getNum());
+//                    }
+//                }
+//            }
+//
+//            System.out.println("对比编码及名称后，总表中存在表1的记录数:"+ret.size());
+//            System.out.println(ret.toString());
 
 
         }catch (Exception e){
