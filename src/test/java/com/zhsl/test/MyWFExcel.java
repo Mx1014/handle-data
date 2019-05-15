857ceb55547cd0e14c4a5b876e44ac879a0bbb54
xiangjg <1991887681@qq.com>
2019-05-15 08:26:56 +0800

    init

--- src/test/java/com/zhsl/test/MyWFExcel.java
+++ src/test/java/com/zhsl/test/MyWFExcel.java
@@ -2,13 +2,26 @@ package com.zhsl.test;
 
 import com.zhsl.data.entity.WFVo;
 import com.zhsl.data.jdbc.mysql.JDBCTools;
+import com.zhsl.data.jdbc.pg.PgJdbc;
 import com.zhsl.data.util.ExcelUtil;
 import org.apache.commons.lang.StringUtils;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.Color;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author xiangjg
@@ -17,104 +30,102 @@ import java.util.List;
 public class MyWFExcel {
 
     @Test
-    public void test(){
+    public void inDB(){
+        java.sql.Connection con = null;
+        java.sql.Statement sta = null;
         try {
+            con = PgJdbc.getConnection();
+            sta = con.createStatement();
+
             ExcelUtil eu = new ExcelUtil();
-            eu.setExcelPath("D:\\查重复值.xlsx");
+            eu.setExcelPath("D:\\666.xlsx");
             eu.setSelectedSheetIdx(0);
             List<Row> list = eu.readExcel();
-            List<List<String>> myList = new ArrayList<>();
+            int type = 0;
             for (int i = 0; i < list.size(); i++) {
                 if (i > 0) {
-                    List<String> tmp = new ArrayList<>();
-                    tmp.add(String.valueOf(i));
-                    for (int j = 2; j < 4; j++) {
-                        String value = ExcelUtil.getCellValueForCell(eu.wb, list.get(i).getCell(j));
-                        tmp.add(value.trim());
-                    }
-                    myList.add(tmp);
-                }
-            }
-            //System.out.println(myList.toString());
-
-            List<WFVo> allList = getList(myList);
-
-
-            eu.setSelectedSheetIdx(1);
-            list = eu.readExcel();
-            myList = new ArrayList<>();
-            for (int i = 0; i < list.size(); i++) {
-                if (i > 0) {
-                    List<String> tmp = new ArrayList<>();
-                    tmp.add(String.valueOf(i));
-                    for (int j = 0; j < 2; j++) {
-                        String value = ExcelUtil.getCellValueForCell(eu.wb, list.get(i).getCell(j));
-                        tmp.add(value.trim());
-                    }
-                    myList.add(tmp);
-                }
-            }
-            //System.out.println(myList.toString());
-
-            List<WFVo> oneList = getList(myList);
-
-            List<Integer> ret = new ArrayList<>();
-            List<WFVo> tmpList = getList(myList);
-            for (WFVo obj1:oneList
-                 ) {
-                for (WFVo obj2:allList
-                        ) {
-                    if(obj1.getCode().equals(obj2.getCode())){
-                        tmpList.add(obj1);
-                        ret.add(obj1.getNum());
+                    String code = ExcelUtil.getCellValueForCell(eu.wb, list.get(i).getCell(0));
+                    String name = ExcelUtil.getCellValueForCell(eu.wb, list.get(i).getCell(1));
+                    String price = ExcelUtil.getCellValueForCell(eu.wb, list.get(i).getCell(2));
+                    if (!StringUtils.isEmpty(code)) {
+                        String sql = String.format("insert into wf_table(num,code,name,tb,price) values(%s,'%s','%s',%s,%s)",
+                                i, code, name, type, price);
+                        System.out.println(sql);
+                        JDBCTools.execute(sql);
                     }
                 }
             }
-            System.out.println("对比编码后，总表中存在表1的记录数:"+ret.size());
-            System.out.println("其中重复：");
-            for (WFVo obj1:tmpList
-                    ) {
-                for (WFVo obj2:tmpList
-                        ) {
-                    if(obj1.getNum()!=obj2.getNum()&&obj1.getCode().equals(obj2.getCode())){
-                        System.out.println("第 "+obj1.getNum()+"行编码：["+obj1.getCode()+"]  和  第 "+obj2.getNum()+ " 行编码：["+obj1.getCode()+"] 重复");
-                    }
-                }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (sta != null)
+                    sta.close();
+                if (con != null)
+                    con.close();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            System.out.println(ret.toString());
-
-            ret = new ArrayList<>();
-
-            for (WFVo obj1:oneList
-                    ) {
-                for (WFVo obj2:allList
-                        ) {
-                    if(obj1.getCode().equals(obj2.getCode())&&obj1.getName().equals(obj2.getName())){
-                        ret.add(obj1.getNum());
-                    }
-                }
-            }
-
-            System.out.println("对比编码及名称后，总表中存在表1的记录数:"+ret.size());
-            System.out.println(ret.toString());
-
-
-        }catch (Exception e){
 
         }
     }
+    @Test
+    public void test() {
+        java.sql.Connection con = null;
+        java.sql.Statement sta = null;
+        try {
+            con = PgJdbc.getConnection();
+            sta = con.createStatement();
 
-    private List<WFVo> getList(List<List<String>> myList){
-        List<WFVo> list = new ArrayList<>();
-        for (List<String> l:myList
-                ) {
-            WFVo vo = new WFVo();
-            vo.setNum(Integer.valueOf(l.get(0))+1);
-            vo.setCode(l.get(1));
-            vo.setName(l.get(2));
-            if(!StringUtils.isEmpty(l.get(1)))
-                list.add(vo);
+            int tb = 1;
+            File file = new File("D:\\666.xlsx");
+            FileInputStream inputStream = new FileInputStream(file);
+            XSSFWorkbook xssfWorkbook = new XSSFWorkbook(inputStream);
+            Sheet sheet = xssfWorkbook.getSheetAt(0);
+            XSSFCellStyle style = xssfWorkbook.createCellStyle();
+            style.setFillForegroundColor(new XSSFColor(java.awt.Color.RED));
+            style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+            style.setBorderTop(BorderStyle.THIN);
+            style.setBorderBottom(BorderStyle.THIN);
+            style.setBorderLeft(BorderStyle.THIN);
+            style.setBorderRight(BorderStyle.THIN);
+            //myList = new ArrayList<>();
+            List<Map<String, String>> list = JDBCTools.query("select t1.* from wf_table t1 JOIN wf_table t2 on t1.code=t2.code where t1.tb=0 and t2.tb="+tb+" t1.price=t2.price");
+            for (Map<String, String> obj:list
+                 ) {
+                int index = Integer.valueOf(obj.get("num"));
+                for (int j = 0; j < 15; j++) {
+                    sheet.getRow(index).getCell(j).setCellStyle(style);
+                }
+            }
+//            XSSFCellStyle redStyle = xssfWorkbook.createCellStyle();
+//            redStyle.setFillForegroundColor(new XSSFColor(java.awt.Color.RED));
+//            redStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+//            list = JDBCTools.query("select t1.* from wf_table t1 JOIN wf_table t2 on t1.code=t2.code where t1.tb=0 and t2.tb="+tb+" and t1.price<>t2.price");
+//            for (Map<String, String> obj:list
+//                    ) {
+//                int index = Integer.valueOf(obj.get("num"));
+//                for (int j = 10; j < 16; j++) {
+//                    sheet.getRow(index).getCell(j).setCellStyle(redStyle);
+//                }
+//            }
+            //对修改后的Excel进行保存
+            FileOutputStream excelFileOutPutStream = new FileOutputStream(file.getAbsolutePath());
+            xssfWorkbook.write(excelFileOutPutStream);
+            excelFileOutPutStream.flush();
+            excelFileOutPutStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (sta != null)
+                    sta.close();
+                if (con != null)
+                    con.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
-        return list;
     }
 }
